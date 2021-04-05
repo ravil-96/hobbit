eca5d5cf999b5e19b3946d5e098509f33a55bbb6
Roselyn Le <roselynle95@gmail.com>
2021-04-05 13:38:53 +0100

    add controllers for users

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,4 +2,7 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users')
 
+router.get('/', usersController.index)
+router.get('/:username', usersController.show)
+
 module.exports = router;
