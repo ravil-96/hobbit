0575ffe6bbe9736631c43a4d09bbe44cb50e81cb
Roselyn Le <roselynle95@gmail.com>
2021-04-05 14:44:34 +0100

    create auth route

---
+++ api/controllers/auth.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+async function register(req, res) {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashed = await bcrypt.hash(req.body.password, salt)
+        await User.create({...req.body, password: hashed})
+        res.status(201).json({msg: 'User created'})
+    } catch (err) {
+        res.status(500).json({err});
+    }
+}
+
+async function login(req, res) {
+    try {
+        const user = await User.findByUsername(req.body.username)
+        if(!user){ throw new Error('No user with this username') }
+        const authed = bcrypt.compare(req.body.password, user.passwordDigest)
+        if (!!authed){
+            const payload = { username: user.username }
+            const sendToken = (err, token) => {
+                if(err){ throw new Error('Error in token generation') }
+                res.status(200).json({
+                    success: true,
+                    token: token,
+                });
+            }
+            jwt.sign(payload, process.env.SECRET, { expiresIn: 1000 }, sendToken);
+        } else {
+            throw new Error('User could not be authenticated')  
+        }
+    } catch (err) {
+        res.status(401).json({ err });
+    }
+}
+
+module.exports = { register, login };
