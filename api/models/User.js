1e1aa1e47e96fbb31279df9bb356a6c1d3498c06
HeyAero <aeroyts@icloud.com>
2021-04-05 11:53:46 +0100

    add get all method to user model

--- api/models/User.js
+++ api/models/User.js
@@ -7,6 +7,18 @@ class User {
     this.passwordDigest = data.password_digest
     this.daily_streak = 0;
   }
+
+  static get all() {
+    return new Promise(async (res, rej) => {
+      try {
+        let result = await db.run(SQL`SELECT * FROM users;`);
+        let users = result.row.map(r => new User(r));
+        res(users);
+      } catch (err) {
+        rej(`ERROR: Could not retrieve users - ${err}`);
+      }
+    })
+  }
 }
 
 module.exports = User;
