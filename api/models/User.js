16d7befe817d5a9c6f382974f85fa2161af100e0
HeyAero <aeroyts@icloud.com>
2021-04-05 11:58:53 +0100

    add findByUsername method to user model

--- api/models/User.js
+++ api/models/User.js
@@ -30,6 +30,18 @@ class User {
       }
     })
   }
+
+  static findByUsername(username) {
+    return new Promise(async (res, rej) => {
+      try {
+        let result = await db.run(SQL`SELECT * FROM users WHERE username = ${username};`)
+        let user = new User(result.rows[0]);
+        res(user);
+      } catch (err) {
+        rej(`ERROR: Retrieving username - ${err}`)
+      }
+    });
+  }
 }
 
 module.exports = User;
