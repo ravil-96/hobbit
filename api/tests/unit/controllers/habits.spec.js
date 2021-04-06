56509c571525c64a4af7f1013a41c0f1aa4a83b1
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-06 17:43:27 +0100

    adding unit test for index method of habit model

--- api/tests/unit/controllers/habits.spec.js
+++ api/tests/unit/controllers/habits.spec.js
@@ -4,4 +4,22 @@ const Habit = require('../../../models/Habit');
 const mockSend = jest.fn();
 const mockJson = jest.fn();
 const mockStatus = jest.fn(code => ({ send: mockSend, json: mockJson, end: jest.fn() }))
-const mockRes = { status: mockStatus}
+const mockRes = { status: mockStatus }
+
+describe('habits controller', () => {
+    beforeEach(() => jest.clearAllMocks());
+
+    afterAll(() => jest.resetAllMocks());
+
+    describe('index', () => {
+        test('it returns habits with a 200 status code', async () => {
+            let testHabits = [//insert examples of habits]
+            jest.spyOn(Habit, 'all', 'get')
+                 .mockResolvedValue(testHabits);
+            await habitsController.index(null, mockRes);
+            expect(mockStatus).toHaveBeenCalledWith(200);
+            expect(mockJson).toHaveBeenCalledWith(testHabits);
+        })
+    });
+
+})
