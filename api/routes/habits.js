43a8b75c2ff1713bbbbca7585a300f2c019f5d7c
Roselyn Le <roselynle95@gmail.com>
2021-04-06 12:40:11 +0100

    update habit controller fucntion in routes

--- api/routes/habits.js
+++ api/routes/habits.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const habitsController = require('../controllers/habits')
 const { verifyToken } = require('../middleware/auth');
 
-router.get('/:user_id', verifyToken, habitsController.show)
-router.post('/', verifyToken, habitsController.create)
-router.delete('/:id', verifyToken, habitsController.destroy)
+router.get('/', habitsController.index)
+router.get('/:user_id', verifyToken, habitsController.showUser)
+router.post('/', verifyToken, habitsController.createHabit)
+router.delete('/:id', verifyToken, habitsController.destroyHabit)
 
 module.exports = router;
