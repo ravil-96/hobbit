2cdbb0aeb38b7aa02e797a9710e8cf72ac7a88be
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-06 17:05:10 +0100

    starting unit testing of habits controller by setting mocking flow

---
+++ api/tests/unit/controllers/habits.spec.js
@@ -0,0 +1,7 @@
+const habitsController = require('../../../controllers/habits')
+const Habit = require('../../../models/Habit');
+
+const mockSend = jest.fn();
+const mockJson = jest.fn();
+const mockStatus = jest.fn(code => ({ send: mockSend, json: mockJson, end: jest.fn() }))
+const mockRes = { status: mockStatus}
