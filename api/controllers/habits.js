3394b495b405a16e3ce1b988447efeb3011ee7c5
Roselyn Le <roselynle95@gmail.com>
2021-04-06 12:41:50 +0100

    fix habit controllers

--- api/controllers/habits.js
+++ api/controllers/habits.js
@@ -1,12 +1,40 @@
 const Habit = require("../models/Habit");
 
-async function show(req, res) {
+async function index (req, res) {
     try {
-        const habit = await Habit.findByUserId(req.params.user_id);
+        const habits = await Habit.all;
+        res.status(200).json(habits)
+    } catch (err) {
+        res.status(500).json({err})
+    }
+}
+
+async function showUser(req, res) {
+    try {
+        const user = await Habit.findByUserId(req.params.user_id);
         res.status(200).json(user);
     } catch (err) {
         res.status(404).json({ err });
     }
 }
 
-module.exports = { show , create };
+async function createHabit(req, res) {
+      try {
+        const habit = await Habit.create(req.body);
+        res.status(200).json(habit)
+    } catch (err) {
+        res.status(422).json({err})
+    }
+}
+
+async function destroyHabit(req, res) {
+      try {
+        const habit = await Habit.findHabitById(req.params.id);
+        const resp = await habit.destroy();
+        res.status(204).end();
+    } catch (err) {
+        res.status(404).json({err});
+    };
+}
+
+module.exports = { index , showUser , createHabit , destroyHabit };
