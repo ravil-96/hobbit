967fc234293de34721401a570fa4ad221ca30b9f
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-07 16:21:21 +0100

    completing config file so that test table are reset each time before fresh test

---
+++ api/tests/integration/config.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+const fs = require('fs');
+
+const request = require('supertest');
+const apiServer = require('../../server');
+
+const reset = fs.readFileSync(_dirname + '/reset.sql').toString();
+
+const resetTestDB = () => {
+    return new Promise (async (res, rej) => {
+        try {
+            const db = new Pool();
+            await db.query(reset)
+            res('Test DB reset')
+        } catch(err){
+            rej('Could not reset TestDB')
+        }
+    }) 
+}
+
+global.request = request
+global.app = apiServer
+global.resetTestDB = resetTestDB
+
