d6996e047918027789b1ec9b9954b461a86209aa
HeyAero <aeroyts@icloud.com>
2021-04-07 10:53:16 +0100

    update renderHabits function and update index html to correct formatting

--- client/js/habits.js
+++ client/js/habits.js
@@ -1,23 +1,40 @@
 async function renderHabits() {
   const feed = document.getElementById('habbit-list');
   const habits = document.createElement('div');
+  const testData = [{
+    id: 1,
+    name: "Water Break",
+    habit_desc: "Take a water break",
+    frequency: "daily",
+    streak_track: 4,
+    streak_start: "test",
+    streak_end: "test",
+    user_id: 1
+  }]
   const userHabits = await getAllHabbits();
 
   const allHabits = (habitData) => {
     const habit = document.createElement('div');
     habit.id = habitData.id;
-    const name = document.createElement('h5');
+    habit.className = "single-habit";
+    const name = document.createElement('h3');
     name.textContent = habitData.name;
     const desc = document.createElement('p');
     desc.textContent = habitData.habit_desc;
     const freq = document.createElement('p');
-    freq.textContent = `Frequency: ${freq}`;
+    freq.textContent = `Frequency: ${habitData.frequency}`;
     const track = document.createElement('p');
     track.textContent = habitData.streak_track;
     const startDate = document.createElement('p');
     startDate.textContent = habitData.streak_start;
     const endDate = document.createElement('p');
     endDate.textContent = habitData.streak_end;
+    const checkBoxLabel = document.createElement('label');
+    checkBoxLabel.for=`complete-${habitData.id}`;
+    const checkBox = document.createElement('input');
+    checkBox.type = "checkbox";
+    checkBox.id = `complete-${habitData.id}`;
+    checkBox.name = `complete-${habitData.id}`;
 
     habit.appendChild(name);
     habit.appendChild(desc);
@@ -25,6 +42,7 @@ async function renderHabits() {
     habit.appendChild(track);
     habit.appendChild(startDate);
     habit.appendChild(endDate);
+    habit.appendChild(checkBox);
 
     habits.appendChild(habit);
   }
@@ -32,3 +50,5 @@ async function renderHabits() {
   userHabits.forEach(allHabits);
   feed.appendChild(habits);
 }
+
+renderHabits();
