4dcbee77dbdc06b54189a8403b9ba6c954b9273e
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-08 16:29:57 +0100

    attempting refactoring of the test suites to include token flow so that tests are allowed into api

--- api/tests/integration/usersRoutes.spec.js
+++ api/tests/integration/usersRoutes.spec.js
@@ -1,12 +1,24 @@
+const { request } = require("express");
+
 describe('users endpoints', () => {
     let api;
     beforeEach(async () => {
         await resetTestDB()
     })
 
-    beforeAll(async () => {
-        api = app.listen(5000, () => console.log('Test server running on port 5000'))
-    });
+    beforeAll(async (done) => {
+        api = app.listen(5000, () => console.log('Test server running on port 5000'));
+        request(api)
+           .post('/login')
+           .send({
+               username: 'User1',
+               password_digest: 'hihuyyftcg5r5456576',
+           })
+           .end((err, res) => {
+            token = res.body.token;
+            done();
+        });
+    })
 
     afterAll(async () => {
         console.log('Gracefully stopping test server')
@@ -16,14 +28,20 @@ describe('users endpoints', () => {
     // also add test for index route 
     it('should retrieve all users', async () => {
         const res = await request(api).get('/users')
-        expect(res.statusCode).toEqual(200)
-        expect(res.body).toHaveLength(2)
-    })
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => {
+          expect(res.statusCode).toEqual(200)
+          expect(res.body).toHaveLength(2)
+        })
+    });
 
     it('should retrieve a user based on id', async () => {
         const res = await request(api).get('/users/1')
-        expect(res.statusCode).toEqual(200)
-        expect(res.body.name).toEqual('User1')
+        .set('Authorization', `Bearer ${token}`)
+        .then((res) => {
+          expect(res.statusCode).toEqual(200)
+          expect(res.body.name).toEqual('User1')    
+        });
     });
 
 })
