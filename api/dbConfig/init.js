920c8189399521968c7637ae04ed6d3c8a082df5
HeyAero <aeroyts@icloud.com>
2021-04-08 16:20:47 +0100

    update for heroku

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,5 +1,19 @@
 const { Pool } = require("pg");
 
+process.env.DATABASE_URL
+
+let config;
+
+if(process.env.DATABASE_URL) {
+  config = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
+}
+
+
 const pool = new Pool();
 
 module.exports = pool;
