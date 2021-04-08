bcc74428c7881cfc754fa05c5b3eddc618868ed6
Roselyn Le <roselynle95@gmail.com>
2021-04-08 18:50:55 +0100

    fix typon in reset db file

--- api/tests/integration/reset.sql
+++ api/tests/integration/reset.sql
@@ -1,12 +1,12 @@
 TRUNCATE users, habits RESTART IDENTITY;
 
-INSERT INTO users (usename, password_digest)
+INSERT INTO users (username, password_digest)
 VALUES 
     ('User1', 'hihuyyftcg5r5456576'),
     ('User2', 'uihihuhihid545457f6');
 
-INSERT INTO habits (habit_desc, frequency, streak_track, streak_start, streak_end, user_id)
+INSERT INTO habits (name, habit_desc, frequency, streak_track, streak_end, streak_complete, user_id)
 VALUES
-    ('Bla Bla Habit', 'daily', 4, 'unknown', 'unknown', 1 ),
-    ('Hey Hey Habit', 'weekly', 5, 'unknown', 'unknown', 2),
-    ('La La Habit', 'weekly', 1, 'unknown', 'unknown', 1);
+    ('Bla', 'Bla Bla Habit', 'daily', 4, 'unknown', 'unknown', 1),
+    ('Hey', 'Hey Hey Habit', 'weekly', 5, 'unknown', 'unknown', 2),
+    ('La', 'La La Habit', 'weekly', 1, 'unknown', 'unknown', 1);
